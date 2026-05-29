693469f2618c28205ac3522a033db7daf34ce878
Alloy Growth Partners <admin@alloygp.co>
2026-05-29 17:40:07 -0500

    feat: add review widget config (dormant)

---
+++ src/config/review.ts
@@ -0,0 +1,10 @@
+export const PASTEL_BASE = 'https://usepastel.com/link/opjkd9ro/#';
+export const TICKET_ID   = '';
+
+export interface ReviewItem {
+  label: string;
+  path: string;
+  review: boolean;
+}
+
+export const REVIEW_ITEMS: ReviewItem[] = [];
